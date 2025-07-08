efd78053650dcde02f0a4e26e66f70346d3cba86
AntohaDontLookAtMe <powerwolfgdansk@gmail.com>
2025-07-08 09:36:53 +0400

    Доработка отображения консоли

--- EmployeeApp/EmployeeApp/Program.cs
+++ EmployeeApp/EmployeeApp/Program.cs
@@ -2,6 +2,7 @@
 using EmployeeApp.Models;
 using EmployeeApp.Services;
 using EmployeeApp.Exceptions;
+using System.Threading;
 
 namespace EmployeeApp
 {
@@ -14,6 +15,7 @@ namespace EmployeeApp
             manager.LoadJSON();
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("\nМеню:");
                 Console.WriteLine("1. Добавить сотрудника");
                 Console.WriteLine("2. Получить информацию о сотруднике");
@@ -24,6 +26,8 @@ namespace EmployeeApp
                 switch (Console.ReadLine())
                 {
                     case "1":
+                        Console.Clear();
+                        Console.WriteLine("-----ДОБАВЛЕНИЕ СОТРУДНИКА----\n");
                         Console.Write("Имя: ");
                         string name = Console.ReadLine();
 
@@ -36,25 +40,36 @@ namespace EmployeeApp
                             decimal salary = decimal.Parse(Console.ReadLine());
                             manager.AddEmployee(new FullTimeEmployee { Name = name, BaseSalary = salary });
                         }
-                        else
+                        else if (type == "2")
                         {
                             Console.Write("Почасовая ставка: ");
                             decimal rate = decimal.Parse(Console.ReadLine());
                             manager.AddEmployee(new PartTimeEmployee { Name = name, HourRate = rate});
                         }
+                        else
+                        {
+                            Console.WriteLine("Неверный ввод");
+                            Thread.Sleep(3000);
+                        }
                         break;
                     case "2":
+                        Console.WriteLine("-----ПОЛУЧЕНИЕ ИНФОРМАЦИИ О СОТРУДНИКЕ----\n");
+                        Console.Clear();
                         Console.WriteLine("Введите имя сотрудника: ");
                         string findingName = Console.ReadLine();
                         manager.GetEmployee(findingName);
                         break;
                     case "3":
+                        Console.Clear();
+                        Console.WriteLine("-----ОБНОВЛЕНИЕ ИНФОРМАЦИИ О СОТРУДНИКЕ----\n");
                         Console.WriteLine("Введите имя сотрудника: ");
                         findingName = Console.ReadLine();
                         Employee findingEmployee = manager.GetEmployee(findingName);
                         manager.UpdateEmployee(findingEmployee);
                         break;
                     case "4":
+                        Console.Clear();
+                        Console.WriteLine("-----УДАЛЕНИЕ СОТРУДНИКА----\n");
                         Console.WriteLine("Введите имя сотрудника: ");
                         findingName = Console.ReadLine(); 
                         manager.DeleteEmployee(findingName);
